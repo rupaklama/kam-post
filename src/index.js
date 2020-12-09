299bf6e8b85bf6172bad5b9abf34f4d2672279e5
Rupak <rupaklamapythondeveloper@gmail.com>
2020-12-08 21:33:52 -0800

    initial commit with hot module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Hot module replacement to load our code faster instead of full browser refresh 
+// after making changes in our code. 
+const rootElement = document.getElementById('root');
+
+const render = () => {
+  ReactDOM.render(<App />, rootElement);
+}
+
+// check to see if hot module is available
+if (module.hot) {
+  // first arg - pass in reference to where our app component is located
+  // second arg - render function from above
+  module.hot.accept('./App', () => {
+    setTimeout(render)
+  })
+}
+
+// calling our func
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
